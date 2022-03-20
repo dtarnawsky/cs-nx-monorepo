470ad1fdf18b69d27ae031bf410fe3129a867836
Damian Tarnawsky <damian@ionic.io>
2022-03-20 07:39:11 -0700

    (chore) add native ios/android projects

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'my.org',
+  appName: 'my-org',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
